4b2e2288e135b71949e2f503bd2e7c6b3a17d9fc
Marcelo Negreiros <marcelo.negreiros@mbold.net>
2024-08-27 14:21:11 -0300

    v 1.1

--- genpwd.py
+++ genpwd.py
@@ -1,32 +1,26 @@
 import base64
 import os
 import os.path
-import sys
-import datetime
-import random
-import hashlib
-import platform
 
 # ------ Cryptographics functions
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives.hmac import HMAC
 
-__syspwd__: str = "3rM%3DSffQmpXcu9AJvmql09x6Wd5zYkpHs"  # Your Default System Key
+__syspwd__: bytes = b"3rM%3DSffQmpXcu9AJvmql09x6Wd5zYkpHs"  # Your Default System Key
 
 
-def getKey(login_password=""):
+def getKey(yourkey=""):
     """
     create the cryptographic seed object for all system or to a user with a login and password
-    MB - 29/12/2023
+    MN - 29/12/2023
     Returns the key (bytes) for use with Fernet
     """
 
-    if len(login_password) == 0:
-        message = __syspwd__.encode(encoding="utf-8")
+    if len(yourkey) == 0:
+        message = __syspwd__
     else:
-        message = login_password.encode(encoding="utf-8")
+        message = yourkey.encode()
 
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -42,33 +36,25 @@ def getKey(login_password=""):
 
 def main():
 
-    _exit: bool = False
-    while not _exit:
-
-        user_name = input("Give me your best email: ")
-        user_pwd = input("Now, put a good (secure) password: ")
-
-        if len(user_name) != 0 and len(user_pwd) != 0:
-            if (
-                input(f"    {user_name},    {user_pwd}       Confirm (y/n)? ").upper()
-                == "Y"
-            ):
-                _exit = True
-
-    print(
-        f"Generating the key derivated from your login:\n  [{user_name} and {user_pwd}]:"
+    mykey = input(
+        "Enter your Key to generate password or \n   <Enter> to use the program key : "
     )
 
-    _key = getKey(user_name + user_pwd)
+    _key = getKey(mykey)
     f = Fernet(_key)
 
-    _astring: str = "The House is green"
+    _astring: str = ""
 
     print(f"\nKey Generated: {_key.decode('utf-8')}")
+    print(
+        "You can encrypt and decrypt any data using this generated key.\nHere is an example:"
+    )
+
+    _astring = input("Type a phrase: ")
 
     _stringEnc = f.encrypt(_astring.encode())
 
-    print(f"Encrypting string {_astring} with the key above: {_stringEnc.decode()}")
+    print(f"\nEncrypting string {_astring} with the key above: {_stringEnc.decode()}")
     print(
         f"Decrypting string {_stringEnc.decode()} to get the phrase: {f.decrypt(_stringEnc).decode()}"
     )
